03f5a106484ad83ecdc1958c717fa3b92d1525a3
Adrián Maza <adri_mv1@hotmail.com>
2024-06-28 12:52:32 +0200

    display tasks

--- src/context/task.context.jsx
+++ src/context/task.context.jsx
@@ -2,11 +2,43 @@ import { createContext, useState } from "react";
 
 const TaskContext = createContext();
 
+const taskList = [
+  {
+    id: "1",
+    title: "Comprar la cena",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Cocinar",
+    completed: false,
+  },
+  {
+    id: "3",
+    title: "Cenar",
+    completed: false,
+  },
+  {
+    id: "4",
+    title: "Lavar los platos",
+    completed: false,
+  },
+];
+
 function TaskProviderWrapper(props) {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([...taskList]);
+
+  const updateTask = (updatedTask) => {
+    const uptadedTasks = tasks.map((task) => {
+      if (task.id !== updatedTask.id) return task;
+      return updatedTask;
+    });
+
+    setTasks(uptadedTasks);
+  };
 
   return (
-    <TaskContext.Provider value={{ tasks, setTasks }}>
+    <TaskContext.Provider value={{ tasks, setTasks, updateTask }}>
       {props.children}
     </TaskContext.Provider>
   );
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,10 +1,25 @@
+import "./TasksPage.css";
+import { useContext } from "react";
 import HeaderComponent from "../components/HeaderComponent";
+import { TaskContext } from "../context/task.context";
+import TaskCard from "../components/TaskCard";
 
 function TasksPage() {
+  const { tasks } = useContext(TaskContext);
+
+  const taskCards = tasks.map((task) => (
+    <li key={task.id}>
+      <TaskCard task={task}></TaskCard>
+    </li>
+  ));
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
-      <h2>Tasks</h2>
+      <section id="tasks-page">
+        <h2 className="title">Tasks</h2>
+        <ul className="task-list">{taskCards}</ul>
+      </section>
     </>
   );
 }
